96fbc31d0348fe261de1430e20347586ce2ff21c
Hansel Sugarfest <anugrahhusen@gmail.com>
2021-02-21 00:12:12 +0700

    Modified ScanMode

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
-    private Rigidbody getRigidBody;
+    //private Rigidbody getRigidBody;
     [SerializeField] LayerMask interactLayer;
-    private GameObject[] interactiveObjects;
+    private Interactable[] interactiveObjects;
     private float[] eachDistance;
     // Start is called before the first frame update
     void Start()
     {
-        getRigidBody = GetComponent<Rigidbody>();
+        //getRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position,4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 4f, interactLayer);
+        int i = 0;
+        Interactable nearest = null;
+        float nearDist = 9999;
+        while (i < hitColliders.Length)
+        {
+            float thisDist = (transform.position - hitColliders[i].transform.position).sqrMagnitude;
+            if (thisDist < nearDist)
+            {
+                nearDist = thisDist;
+                nearest = hitColliders[i].GetComponent<Interactable>();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E) && nearest != null)
+        {
+            // nearest.DoSomething();
+            Debug.Log("Interaction!");
+            nearest.DoSomething();
+        }
     }
 }
