7668afcf36ceb2d093e6f9058b0e7e09a3078115
Oksidian Tafly <oksitafly@gmail.com>
2021-04-13 20:23:17 +0800

    new SaveLoad UI

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,34 +8,6 @@ public class PauseMenu : MonoBehaviour
     public static int PauseState;
     public GameObject pauseMenuUI;
 
-    private void Start()
-    {
-        setPauseState(0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            setPauseState(3);
-        }
-    }
-
-    public void setPauseState(int state)
-    {
-        if (state == 0)
-        {
-            pauseMenuUI.SetActive(false);
-            Time.timeScale = 1f;
-        }
-        else if (state == 3)
-        {
-            pauseMenuUI.SetActive(true);
-            Time.timeScale = 0f;
-        }
-    }
-
     public void loadMenu()
     {
         SceneManager.LoadScene("MainMenu");
--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -16,6 +16,7 @@ public class InventoryUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        inventory = new List<Obtainable>();
 
         foreach (Image img in invImages)
         {
