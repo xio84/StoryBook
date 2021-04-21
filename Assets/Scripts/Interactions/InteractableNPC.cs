2bc055b0bb6c646c875a5341ab71c75ebe8c44b9
Ferdy <13517116@std.stei.itb.ac.id>
2021-04-21 13:08:08 +0700

    Integrate Map with puzzle & NPC

---
+++ Assets/Scripts/Interactions/InteractableNPC.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableNPC : MonoBehaviour, IInteractables
+{
+
+    public DialogueTrigger dialogueTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool IInteractables.Interact(string key)
+    {
+        dialogueTrigger.TriggerDialogue();
+        Debug.Log("interacted");
+        return true;
+    }
+}
